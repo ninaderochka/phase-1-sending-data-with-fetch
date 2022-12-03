c3a62e34068619a2b1111b417eba046113e6d407
Nina Hawari <nina.hawari@gmail.com>
2022-12-04 01:24:06 +0200

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users" , {
+
+    method: "POST",
+
+        headers: { 
+            "Content-Type" : 'application/json',
+            Accept : 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        } )
+    } )
+    .then (function (res) {
+        return res.json()
+    })
+
+    .then (function(newObject) {
+        document.body.innerHTML = newObject["id"]
+      })
+      .catch((e) => {
+        document.body.innerHTML = ("Unauthorized Access" , e.message);
+})}
+
+
+
